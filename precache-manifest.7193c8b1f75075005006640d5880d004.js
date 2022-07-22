dbd81d3fc6dcd75b8f07802978ddc9b91a717502
Rodrigo Torres <torresperez12@hotmail.com>
2022-07-22 16:24:21 -0700

    Updates

---
+++ precache-manifest.7193c8b1f75075005006640d5880d004.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58c6334fef2275e257e",
+    "url": "/my-blog-react/static/css/main.edcf77ef.chunk.css"
+  },
+  {
+    "revision": "e58c6334fef2275e257e",
+    "url": "/my-blog-react/static/js/main.700c2f5f.chunk.js"
+  },
+  {
+    "revision": "41585349487ec537a91d",
+    "url": "/my-blog-react/static/js/runtime~main.c17961bd.js"
+  },
+  {
+    "revision": "df10041a9f3b5a0e2341",
+    "url": "/my-blog-react/static/js/2.5fbcee3b.chunk.js"
+  },
+  {
+    "revision": "bfe4cf5387793658b22a522bb18c9048",
+    "url": "/my-blog-react/static/media/WhatsApp Image 2022-06-29 at 4.57.57 PM.bfe4cf53.jpeg"
+  },
+  {
+    "revision": "cd4b343d75a3b66acbbb8d8fde5df7a6",
+    "url": "/my-blog-react/static/media/El palacio de Buckingham y la programacion.cd4b343d.png"
+  },
+  {
+    "revision": "46a25e678f12de0fbeb7ff475e3f6c0f",
+    "url": "/my-blog-react/static/media/cuarto1.46a25e67.jpg"
+  },
+  {
+    "revision": "c97a1418bc1b8032ec1a7a42015c95e8",
+    "url": "/my-blog-react/static/media/palacio2.c97a1418.jpg"
+  },
+  {
+    "revision": "9b67dbfd80a963759427128d5232cf74",
+    "url": "/my-blog-react/static/media/atomic-design.9b67dbfd.png"
+  },
+  {
+    "revision": "d334e3a35cb89e1b83a7a35e83908ead",
+    "url": "/my-blog-react/static/media/atomic-design2.d334e3a3.jpg"
+  },
+  {
+    "revision": "0e937ac4e9ebd74b8d1945da8b896db0",
+    "url": "/my-blog-react/index.html"
+  }
+];
